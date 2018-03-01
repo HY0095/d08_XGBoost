8406a7b6c7ae7a7e1042fcc11490aeae18edb7d4
Duan <hytc0095@gmail.com>
2018-03-01 10:15:28 +0800

    Update metrics.py
    update

--- metrics.py
+++ metrics.py
@@ -1,1 +1,19 @@
-# -*- coding:utf-8 -*-import numpy as npimport pandas as pd
+# -*- coding:utf-8 -*-
+import numpy as np
+import pandas as pd
+
+def ks(y_true, y_pred):
+  data = pd.DataFrame()
+  data['ytrue'] = y_true
+  data['ypred'] = y_pred
+  
+  ks_data = data.sort_values(by='ypred', ascending=False)
+  good = ks_data['ytrue'] == 1
+  bad = ks_data['ytrue'] == 0
+  
+  good_cf = np.cumsum(good / good.sum())
+  bad_cf = np.cumsum(bad / bad.sum())
+  good_bad_cum = list(np.abs(good_cf - bad_cf))
+  
+  return max(good_bad_cum)
+
